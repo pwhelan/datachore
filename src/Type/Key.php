3479960110b93f869709703e7b9ace3c7ae9384d
Phillip Whelan <pwhelan@mixxx.org>
2015-02-14 03:58:47 -0800

    Remove cruft.

--- src/Type/Key.php
+++ src/Type/Key.php
@@ -35,40 +35,6 @@ class Key implements \Datachore\TypeInterface
 		}
 		
 		return false;
-		if (isset($this->updates[$key]))
-		{
-			if ($this->updates[$key] instanceof \google\appengine\datastore\v4\Key)
-			{
-				$fkey = $this->updates[$key];
-			}
-		}
-		
-		if (!isset($fkey) && isset($this->values[$key]))
-		{
-			$fkey = $this->values[$key]->rawValue();
-		}
-		
-		// @codeCoverageIgnoreStart
-		if (!isset($fkey) || !$fkey instanceof \google\appengine\datastore\v4\Key)
-		{
-			return null;
-		}
-		// @codeCoverageIgnoreEnd
-		
-		if (!isset($this->foreign[$key]))
-		{
-			$kindName = $fkey->getPathElement(0)->getKind();
-			$className = str_replace('_', '\\', $kindName);
-			
-			$model = (new $className)->where('id', '==', $fkey)->first();
-			if ($model)
-			{
-				$this->foreign[$key] = $model;
-				return $model;
-			}
-		}
-		
-		return $this->foreign[$key];
 	}
 	
 	public function set($value)
@@ -77,10 +43,6 @@ class Key implements \Datachore\TypeInterface
 		{
 			case $value instanceof \google\appengine\datastore\v4\Key:
 				$this->_key = $value;
-				return;
-			
-			case $value instanceof \Datachore\Type\Key:
-				$this->_key = $value->key();
 				return;
 			
 			case $value instanceof Model:
